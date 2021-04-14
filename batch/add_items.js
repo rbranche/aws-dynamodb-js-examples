f48a9f4c8bfc43081bd74d68b9168c3ddd5c3755
Ryan Branche <rbranche@apple.com>
2021-04-14 14:04:22 -0600

    Add add_items.js

---
+++ batch/add_items.js
@@ -0,0 +1,43 @@
+// add_items.js
+//
+// Add >101 additional items to the DynamoDB table 
+//
+// Usage: node add_items.js
+
+const { v4: uuidv4 } = require('uuid');
+
+const { BatchWriteItemCommand } = require("@aws-sdk/client-dynamodb");
+const { dbclient, tableName, chunkSize } = require("./dbclient.js");
+
+const run = async () => {
+    let results = [];
+
+    for (let i = 0; i < Math.ceil((100 + 1) / chunkSize); i++) {
+        let items = [];
+
+        for (let k = 0; k < chunkSize; k++) {
+            items.push({
+                PutRequest: {
+                    Item: {
+                        uuid: { S: uuidv4() },
+                        chunk_id: { N: (i * 10) + k }
+                    }
+                }
+            });
+        }
+
+        const params = {RequestItems: {[tableName]: items}};
+
+        results.push(dbclient.send(new BatchWriteItemCommand(params)));
+    }
+
+    for (result of results) {
+        try {
+            const data = await result;
+            console.log("Success, items inserted", data);
+        } catch (err) {
+            console.log("Error", err);
+        }
+    }
+};
+run();
--- batch/dbclient.js
+++ batch/dbclient.js
@@ -6,9 +6,12 @@ const REGION = process.env.REGION || "us-west-2";
 
 var ENDPOINT = process.env.ENDPOINT || null;
 
+console.debug("ENDPOINT = " + ENDPOINT)
+
 if (process.env.ENDPOINT == 'aws') {
     ENDPOINT = null;
 }
 
 exports.dbclient = new DynamoDBClient({ region: REGION, endpoint: ENDPOINT });
 exports.tableName = "batchExample" 
+exports.chunkSize = 25
